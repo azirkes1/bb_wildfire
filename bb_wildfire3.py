bfa7798c1f2651f658c8462f66f52aa4cd550ee6
azirkes1 <azirkes@gmail.com>
2025-08-13 20:15:08 +0000

    edit mobile view

--- bb_wildfire3.py
+++ bb_wildfire3.py
@@ -186,7 +186,7 @@ with st.container():
     /* Desktop / landscape */
     @media (min-width: 769px), (orientation: landscape) {
         .folium-map, iframe {
-            height: 80vh !important; /* slightly taller than before */
+            height: 75vh !important; /* slightly taller than before */
         }
     }
 
